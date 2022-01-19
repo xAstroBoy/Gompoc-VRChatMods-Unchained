c266169cd72e2ef729d6aa6988851e6359895467
xAstroBoy <92211398+xAstroBoy@users.noreply.github.com>
2022-01-19 16:46:13 +0100

    Disarm Integrity Checks generation.

--- Tools/IntegrityCheckWeaver/Program.cs
+++ Tools/IntegrityCheckWeaver/Program.cs
@@ -30,77 +30,9 @@ namespace IntegrityCheckWeaver
                 return 1;
             }
 
-            var dummyOneResource = DummyThree.ProduceDummyThree();
-            var dummyOneName = Utils.CompletelyRandomString() + ".dll";
-            assembly.MainModule.Resources.Add(new EmbeddedResource(dummyOneName, ManifestResourceAttributes.Private, dummyOneResource));
-
-            var dummyThreeResource = DummyThree.ProduceDummyThree();
-            var dummyThreeName = Utils.CompletelyRandomString() + ".dll";
-            assembly.MainModule.Resources.Add(new EmbeddedResource(dummyThreeName, ManifestResourceAttributes.Private, dummyThreeResource));
-
-            var dummyTwoName = Utils.CompletelyRandomString() + ".dll";
-
-            assembly.MainModule.Resources.Remove(assembly.MainModule.Resources.Single(it => it.Name.EndsWith("_dummy_.dll"))); // is replaced
-            assembly.MainModule.Resources.Single(it => it.Name.EndsWith("_dummy2_.dll")).Name = dummyTwoName;
-            
-            var methodRenameMap = CleanMethods(modType);
-
-            foreach (var method in modType.Methods)
-            foreach (var instr in method.Body.Instructions)
-                if (instr.OpCode == OpCodes.Ldstr)
-                {
-                    var value = (string)instr.Operand;
-                    instr.Operand = value switch
-                    {
-                        "_dummy_.dll" => dummyOneName,
-                        "_dummy2_.dll" => dummyTwoName,
-                        "_dummy3_.dll" => dummyThreeName,
-                        _ => methodRenameMap.TryGetValue(value, out var renamed) ? renamed : value
-                    };
-                }
-
             assembly.Write();
 
             return 0;
-        }
-
-        private static readonly HashSet<string> ourNamesToRename = new()
-        {
-            "CheckA",
-            "CheckB",
-            "CheckC",
-            "CheckD",
-            "PatchTest",
-            "ReturnFalse",
-            "CheckWasSuccessful",
-            "MustStayFalse",
-            "MustStayTrue",
-            "RanCheck3",
-            "CheckDummyThree",
-            "ourAnnoyingMessages"
-        };
-
-        private static Dictionary<string, string> CleanMethods(TypeDefinition type)
-        {
-            var result = new Dictionary<string, string>();
-
-            foreach (var method in type.Methods)
-                if (ourNamesToRename.Contains(method.Name))
-                {
-                    var newName = Utils.CompletelyRandomString();
-                    result[method.Name] = newName;
-                    method.Name = newName;
-                }
-
-            foreach (var property in type.Properties) 
-                if (ourNamesToRename.Contains(property.Name))
-                    property.Name = Utils.CompletelyRandomString();
-
-            foreach (var field in type.Fields) 
-                if (ourNamesToRename.Contains(field.Name))
-                    field.Name = Utils.CompletelyRandomString();
-
-            return result;
         }
     }
 }
