5079bb887dcc2d0146ea07f7ead2721f14910ae9
xAstroBoy <92211398+xAstroBoy@users.noreply.github.com>
2022-06-20 15:15:40 +0200

    Fixes

--- ActionMenuUtils/BuildInfo.cs
+++ ActionMenuUtils/BuildInfo.cs
@@ -1,22 +1,11 @@
 ﻿using System.Reflection;
 using ActionMenuUtils;
 using MelonLoader;
-using ModJsonGenerator;
 
 
 
 [assembly: MelonGame("VRChat", "VRChat")]
 [assembly: MelonInfo(typeof(ActionMenuUtils.Main), ModConstants.NAME, ModConstants.VERSION, ModConstants.AUTHOR, ModConstants.DOWNLOAD_LINK)]
-[assembly: ModJsonInfo(
-        140, 
-        "Lets you respawn, go home, rejoin instance and reset you avatar to the default robot or a custom one all using the action menu", 
-        new []{"action menu", "respawn", "go home", "reset avatar"}, 
-        new []{"[ActionMenuApi](https://api.vrcmg.com/v0/mods/201/ActionMenuApi.dll)", "[UIExpansionKit](https://api.vrcmg.com/v0/mods/55/UIExpansionKit.dll)"}, 
-        "Fixed issues with respawn and go home buttons", 
-        "#2ad9f7"
-    )
-]
-
 [assembly:AssemblyVersion(ModConstants.VERSION)]
 [assembly:AssemblyFileVersion(ModConstants.VERSION)]
 [assembly:AssemblyTitle(ModConstants.NAME)]
--- WorldPredownload/BuildInfo.cs
+++ WorldPredownload/BuildInfo.cs
@@ -1,27 +1,11 @@
 ﻿using System;
 using System.Reflection;
 using MelonLoader;
-using ModJsonGenerator;
 using WorldPredownload;
 
 
 [assembly: MelonGame("VRChat", "VRChat")]
 [assembly: MelonInfo(typeof(WorldPredownload.WorldPredownload), ModConstants.NAME, ModConstants.VERSION, ModConstants.AUTHOR, ModConstants.DOWNLOAD_LINK)]
-[assembly: ModJsonInfo(
-        141,
-        "-Lets you predownload worlds from the world info page and friend info page \n" +
-        "-Download Status can be found on hud and as well on the bottom of the quickmenu.\n" +
-        "-Switching world while downloading will cancel current download.\n" +
-        "-Mod now checks to see if world has been downloaded previously on world info page and user info page.\n" +
-        "-Mod is useful if you have particularly bad internet and don't fancy hanging around in the loading screen for a decade.\n" +
-        "-Options to follow predownloads configurable via UIX too",
-        new []{"world", "download", "preload", "predownload"},
-        new []{"[UIExpansionKit](https://api.vrcmg.com/v0/mods/55/UIExpansionKit.dll)"},
-        "Compatibility fix for update and add ability to download worlds via portals (credit to yobson)",
-        "#2ad9f7"
-    )
-]
-
 [assembly:AssemblyVersion(ModConstants.VERSION)]
 [assembly:AssemblyFileVersion(ModConstants.VERSION)]
 [assembly:AssemblyTitle(ModConstants.NAME)]
